57b132b1067ca302c9d703fb8e04161621630970
Alexander Gribowski <grias1994@gmail.com>
2021-11-11 13:54:13 +0300

    add UserIdentity model and service for JWT creation and validation

---
+++ SocNet.Services/AuthenticationManaging/IJwtManagingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocNet.Core.Entities;
+
+namespace SocNet.Services.AuthenticationManaging
+{
+    public interface IJwtManagingService
+    {
+        public string CreateToken(UserIdentity user);
+
+        public int? ValidateToken(string token);
+    }
+}
