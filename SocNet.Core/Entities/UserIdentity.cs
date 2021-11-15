730154e735396cadf868167f2ccb898119aed170
Alexander Gribowski <grias1994@gmail.com>
2021-11-15 14:58:01 +0300

    make username field in UserIdentity unique

--- SocNet.Core/Entities/UserIdentity.cs
+++ SocNet.Core/Entities/UserIdentity.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SocNet.Core.Entities
 {
+    [Index(nameof(UserName), IsUnique = true)]
     public class UserIdentity
     {
         public int Id { get; init; }
